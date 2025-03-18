ed1f36106ae7a9f01c82c22b20de19f9845ecaa0
damien1972 <lepage.damien@gmail.com>
2025-03-17 20:23:33 -0700

    Update index.ts

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,3 +1,6 @@
 // Export all contexts
 export { SessionContext, SessionProvider } from './SessionContext';
 export { AccessibilityContext, AccessibilityProvider } from './AccessibilityContext';
+// Export hooks
+export { useAccessibility } from './useAccessibility';
+export { useSession } from './useSession'; // Add this if you're using useSession as well
